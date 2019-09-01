5a1bf542b5d9164605c78cbf48b04f7ee48ca518
apple <apple@192.168.0.105>
2019-09-01 22:07:51 +0530

    update

--- SpringSecurityExample/src/main/java/com/example/security/configuration/securityConfig.java
+++ SpringSecurityExample/src/main/java/com/example/security/configuration/securityConfig.java
@@ -1,5 +1,8 @@
 package com.example.security.configuration;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,14 +14,30 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class securityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	DataSource dataSource;
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
 		// Here we can have our own authentication on the auth object
 		auth.inMemoryAuthentication()//
-				.withUser("lav").password("lav").roles("manager")//
+				.withUser("lav").password("lav").authorities("role_manager")//
 				.and()//
-				.withUser("joseph").password("joseph").roles("user");
+				.withUser("joseph").password("joseph").authorities("role_user");
+
+		/*- We can configure using JDBC as well 
+		 * 
+		 *  Note: Following table should be available in the DB
+		 *  users-> username, password, enabled
+		 *  authorities -> username, authority
+		 * 
+		 *   // Implementation code
+		 *   auth.jdbcAuthentication()//
+		 *		 .dataSource(dataSource)//
+		 *		 .passwordEncoder(getPasswordEncoder());
+		 */
+
 	}
 
 	@Bean
@@ -34,7 +53,7 @@ public class securityConfig extends WebSecurityConfigurerAdapter {
 
 		http.authorizeRequests()//
 				.antMatchers("/manager").hasRole("manager")//
-				.antMatchers("/user").hasAnyRole("user", "manager")//
+				.antMatchers("/user").access("hasRole('manager') AND hasRole('user')")//
 				.antMatchers("/").permitAll()//
 				.and()//
 				.formLogin();
--- SpringSecurityExample/src/main/java/com/example/security/resources/Resource.java
+++ SpringSecurityExample/src/main/java/com/example/security/resources/Resource.java
@@ -1,5 +1,9 @@
 package com.example.security.resources;
 
+import java.security.Principal;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,13 +16,16 @@ public class Resource {
 	}
 
 	@GetMapping("/user")
-	public String user() {
-		return "Resource is accessed by a user";
+	public String user(Principal user) { // you can get the user details using Principal parameter
+		return "Resource is accessed by a user: " + user.getName();
 	}
 
 	@GetMapping("/manager")
 	public String manager() {
-		return "Resource is accessed by a manager";
+
+		/* you can also get the user details from the security context holder */
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		return "Resource is accessed by a manager: " + auth.getName();
 	}
 
 }
